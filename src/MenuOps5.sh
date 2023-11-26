15c4d003bf715537253b44c565aa763eafd0ca54
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 00:23:00 +0300

    Update MenuOps5.sh

--- src/MenuOps5.sh
+++ src/MenuOps5.sh
@@ -1,48 +1,47 @@
 echo '
-   ╔[NOTICE] Steeper Tools
-  [01] >> Information Gathering: 
-   ├ Collects information from database
-   ├ Find More Information
-   ├ More Information In User
-  [02] >> Exploitation Tools:
-   ├ Selection of tools for operation and hacking
-   ├ Tools for special operations 
-   ├ blackmail utilities And human use
+   ╔[ATTENTION] Daha Dik Araçlar
+     [01] >> Bilgi toplama: 
+      ├ Veritabanından bilgi topluyor
+      ├ Daha Fazla Bilgi Bul
+      ├ Kullanıcıda Daha Fazla Bilgi
+     [02] >> Sömürü Araçları:
+      ├ Operasyon ve hackleme için araçların seçimi
+      ├ Özel operasyonlara yönelik araçlar 
+      ├ şantaj araçları ve insan kullanımı
   [SKIP]┐
-      ┌[03] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
-      │[04] >> Web Attack Tools:  -  Tools for hacking websites and servers
-  [-]─┘[05] >> Cam Hacking Tools:  -  Tools for hacking cams and front camera
-       [06] >> Remote Trojan RAT:  -  Utilities for creating RAT virus
-      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣        
-   ╔[NOTICE] Useful Tools 
-  [07] >> SQL injection Tool:
-   ├ Tools for creating viruses
-   ├ And cracking devices
-  [08] >> SocialMedia Hacking:
-   ├ Tools for hacking social networks
-   ├ And Bruteforce social networks
-  [>]┐
+     ┌[03] >> Sniffing ve Spoofing: - Veri ve veri tabanlarında sahteciliğe yönelik araçlar
+     │[04] >> Web Saldırısı Araçları: - Web sitelerini ve sunucuları hacklemeye yönelik araçla
+   ─┘[05] >> Kamera Hackleme Araçları: - Kameraları ve ön kamerayı hacklemeye yönelik araçlar
+     [06] >> Uzak Truva Atı RAT: - RAT virüsü oluşturmaya yönelik yardımcı programlar
+      [-] ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣        
+  ╔[ATTENTION] Yararlı Araçlar 
+      [07] >> SQL enjeksiyon Aracı:
+       ├ Virüs oluşturma araçları
+       ├ Virüs kırma cihazları
+      [08] >> Sosyal Medya Hacking:
+       ├ Sosyal ağları hacklemek için araçlar
+       ├ Ve Bruteforce sosyal ağları
+       [>]┐
    ┌[SKIP]
-  [09] >> Bombing & Spamming Tools:  -  Utilities for sms bombing And Sending Anonymous SMS
-  [10] >> Vulnerability Analysis:  -  Systems for vulnerability analysis 
-  [11] >> DarkSearch Tools:      -  Tools for search in DarkNet & DeepWeb
-  [12] >> Phishing And IpHack:  -  Tools for phishing and fake websites
-  [13] >> Passworld Attack:    -  Utilities for cracking passwords
-  [14] >> Wordlist Generator:  -  Tools for Genering Worldlist
-  [15] >> XSS Attack Tools:  -  Utilities for Attacking XSS
-  [16] >> Discord leaks:   - Discord mass users information
-  [17] >> Telegram Info: - Mass Telegram users information
+       [09] >> Bombalama ve Spam Araçları:  -  Sms bombalama ve anonim SMS gönderme yardımcı programları
+       [10] >> Güvenlik Açığı Analizi:  -  Güvenlik açığı analizine yönelik sistemler 
+       [11] >> Karanlık Arama Araçları:   -  DarkNet ve DeepWebde arama araçları
+       [12] >> Kimlik Avı ve IpHack:  -  Kimlik avı ve sahte web siteleri için araçlar
+       [13] >> Şifre Saldırısı:    -  Şifre kırmaya yönelik yardımcı programlar
+       [14] >> Kelime Listesi Oluşturucu:  -  Kelime Listesi Oluşturma Araçları
+       [15] >> XSS Saldırı Araçları:  -  XSS Saldırmak için Yardımcı Programlar
+       [16] >> Anlaşmazlık sızıntıları:   - Toplu kullanıcı bilgilerinin discord
+       [17] >> Telegram Bilgisi: - Telegram kullanıcı bilgileri
 
-      ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣
-  [18] >> Other tools: 
-   ├ Separate and other tools for hacking
-  [19] >> Terminal Panel: 
-   ├ Termux control and special features
+      [18] >> Diğer Aletler: 
+       ├ Bilgisayar korsanlığı için ayrı ve diğer araçlar
+      [19] >> Terminal Paneli: 
+       ├ Termux kontrolü ve özel özellikler
   [SKIP]┐
-      ┌[20] >> System Settings:  -  AllHackingTools control and special features
-      │[21] >> System License:  -  View the license in AllHackingTools
-  [-]─┘[22] >> Update System:  -  Update system and all tools & menu 
-       [23] >> About System:  -  View system information and developer
-       [24] >> Exit System:  -  Log out of the AllHackingTools system 
-       [25] >> Log Out Terminal  -  Log out in AllHackingTools And Terminal'|lolcat -p 4.6
+      ┌[20] >> Sistem Ayarları: - alltool kontrolü ve özel özellikler
+      │[21] >> Sistem Lisansı: - Lisansı alltool ile görüntüleyin
+  [-]─┘[22] >> Sistemi Güncelle: - Sistemi ve tüm araçları ve menüyü güncelleyin 
+       [23] >> Sistem Hakkında: - Sistem bilgilerini ve geliştiriciyi görüntüleyin
+       [24] >> Sistemden Çıkış: - Alltool sisteminden çıkış yapın 
+       [25] >> Terminalden Çıkış Yap - alltool ve Terminalde çıkış yap'|lolcat -p 4.6
 
