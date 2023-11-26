c9fd377dbafbb8f9676ab445dcfb63786ec664d1
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 01:49:51 +0300

    Update CheckFolder.py

--- src/CheckFolder.py
+++ src/CheckFolder.py
@@ -7,17 +7,17 @@ C = '\033[36m'
 W = '\033[0m' 
 
 # Secret folders
-path1 = "/data/data/com.termux/files/home/AllHackingTools/.logs/"
-path2 = "/data/data/com.termux/files/home/AllHackingTools/.settings/"
-path3 = "/data/data/com.termux/files/home/AllHackingTools/.temp/"
-path4 = "/data/data/com.termux/files/home/AllHackingTools/.check/"
+path1 = "/data/data/com.termux/files/home/alltool/.logs/"
+path2 = "/data/data/com.termux/files/home/alltool/.settings/"
+path3 = "/data/data/com.termux/files/home/alltool/.temp/"
+path4 = "/data/data/com.termux/files/home/alltool/.check/"
 
 # No secret folders
-path5 = "/data/data/com.termux/files/home/AllHackingTools/Files/"
-path6 = "/data/data/com.termux/files/home/AllHackingTools/src/"
-path7 = "/data/data/com.termux/files/home/AllHackingTools/Tool/"
-path8 = "/data/data/com.termux/files/home/AllHackingTools/Themes/"
-path9 = "/data/data/com.termux/files/home/AllHackingTools/Castom/"
+path5 = "/data/data/com.termux/files/home/alltool/Files/"
+path6 = "/data/data/com.termux/files/home/alltool/src/"
+path7 = "/data/data/com.termux/files/home/alltool/Tool/"
+path8 = "/data/data/com.termux/files/home/alltool/Themes/"
+path9 = "/data/data/com.termux/files/home/alltool/Castom/"
 
 # check folders
 if os.path.exists(path1):
@@ -29,79 +29,79 @@ if os.path.exists(path1):
                         if os.path.exists(path7):
                             if os.path.exists(path8):
                                 if os.path.exists(path9):
-                                    os.system("cd && cd AllHackingTools && cd src && bash starterUp.sh")
+                                    os.system("cd && cd alltool && cd src && bash starterUp.sh")
                                 else:
-                                    print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                                    print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                                    print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                                    print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                                    print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                                    print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                                     print("")
-                                    print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                                    print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                                    print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                                    print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                                     exit(0)
 
                             else:
-                                print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                                print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                                print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                                print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                                print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                                print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                                 print("")
-                                print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                                print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                                print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                                print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                                 exit(0)
                         else:
-                            print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                            print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                            print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                            print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                            print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                            print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                             print("")
-                            print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                            print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                            print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                            print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                             exit(0)
                     else:
-                        print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                        print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                        print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                        print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                        print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                        print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                         print("")
-                        print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                        print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                        print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                        print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                         exit(0)
                 else:
-                    print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                    print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                    print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                    print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                    print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                    print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                     print("")
-                    print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                    print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                    print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                    print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                     exit(0)
             else:
-                print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-                print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-                print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+                print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+                print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+                print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
                 print("")
-                print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-                print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+                print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+                print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
                 exit(0)
         else:
-            print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-            print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-            print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+            print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+            print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+            print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
             print("")
-            print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-            print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+            print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+            print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
             exit(0)
 
     else:
-        print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-        print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-        print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+        print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+        print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+        print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
         print("")
-        print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-        print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+        print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+        print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
         exit(0)
 else:
-    print("Folder .logs doesn't exists!")
-    print(R + '[-] ' + C + 'Error code: 106 DNS server refused to connect')
-    print(R + '[-] ' + C + 'Error code: 404 Not Found! No system folder')
-    print(R + '[-] ' + C + 'Error code: 500 Unable to process request due to folder not found!')
+    print(".logs klasörü mevcut değil!")
+    print(R + '[-] ' + C + 'Hata kodu: 106 DNS sunucusu bağlanmayı reddetti')
+    print(R + '[-] ' + C + 'Hata kodu: 404 Bulunamadı! Sistem klasörü yok')
+    print(R + '[-] ' + C + 'Hata kodu: 500 Klasör bulunamadığı için istek işlenemiyor!')
     print("")
-    print(R + '[-] ' + C + 'Try reinstalling the system, or restoring from a backup!')
-    print(R + '[-] ' + C + 'Unable to start the system! Maybe the system files were damaged!')
+    print(R + '[-] ' + C + 'Sistemi yeniden yüklemeyi veya bir yedekten geri yüklemeyi deneyin!')
+    print(R + '[-] ' + C + 'Sistem başlatılamıyor! Sistem dosyaları zarar görmüş olabilir!')
     exit(0)
