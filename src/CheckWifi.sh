f821c795d88a30cfffb47cbb5d9cca4b8bfb20c4
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 01:30:59 +0300

    Update CheckWifi.sh

--- src/CheckWifi.sh
+++ src/CheckWifi.sh
@@ -7,10 +7,10 @@ green='\033[92m'
 ping -c 1 google.com > /dev/null 2>&1
 sleep 2
 if [[ "$?" == 0 ]]; then
-echo -e "$green[+]-[Internet Connection]............[ True ]"
+echo -e "$green[+]-[İnternet bağlantısı]............[ True ]"
 sleep 1.5
 else
-echo -e "$red[-]-[Internet Connection].........[ False ]"
+echo -e "$red[-]-[İnternet bağlantısı].........[ False ]"
 echo ""
 exit
 exit
