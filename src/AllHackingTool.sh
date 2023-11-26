0768fde6968ad27f58893ad166770f51c53752cb
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 01:59:19 +0300

    Update AllHackingTool.sh

--- src/AllHackingTool.sh
+++ src/AllHackingTool.sh
@@ -1,6 +1,6 @@
 cd
 cd
-cd AllHackingTools
+cd alltool
 python3 src/CheckVersion.py
 python3 .check/CheckServerYesAndNo.py
 sleep 3
@@ -11,11 +11,11 @@ b="\033[1;34m"
 w="\033[0m"
 cd
 cd
-cd AllHackingTools 
+cd alltool 
 cd Tool
-mv MainMenu.py /data/data/com.termux/files/home/AllHackingTools
+mv MainMenu.py /data/data/com.termux/files/home/alltool
 cd
-cd AllHackingTools
+cd alltool
 clear
-echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
+echo -e $b"[ ✔ ]"$g"başarıyla doğrulandı"$w
 python2 MainMenu.py
