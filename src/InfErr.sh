31c64b8d8526d705388beb3e24f4c2c990311209
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 00:55:13 +0300

    Update InfErr.sh

--- src/InfErr.sh
+++ src/InfErr.sh
@@ -4,15 +4,15 @@ b="\033[1;34m"
 w="\033[0m"
 o="\033[1;33m"
 
-echo -e $w"["$g"INFO"$w"]"$b"Starting tool menu. Please wait a moment!"$w
+echo -e $w"["$g"BİLGİ"$w"]"$b"Araç menüsünü . Bir süre bekleyin lütfen!"$w
 sleep 0.06
-echo -e $w"["$o"WARN"$w"]"$b"A don't close termux app! And exit termux!"$w
+echo -e $w"["$o"UYARI"$w"]"$b"Termux uygulamasını kapatmayın! Ve termux'tan çıkmayın!"$w
 sleep 0.07
-echo -e $w"["$r"ERROR"$w"]"$b"Error to starting tool! Error code 25-39"$w
+echo -e $w"["$r"HATA"$w"]"$b"Error to starting tool! Error code 25-39"$w
 sleep 0.02
-echo -e $w"["$g"INFO"$w"]"$b"Reloading Tools. Please wait!"$w
+echo -e $w"["$g"BİLGİ"$w"]"$b"Reloading Tools. Please wait!"$w
 cd
-cd AllHackingTools
+cd alltool
 python2 MainMenu.py
 
 
