def24ffe285b78ad4382686a5e461e4ef2ed6bdb
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 00:52:42 +0300

    Update InstallMenu.py

--- src/InstallMenu.py
+++ src/InstallMenu.py
@@ -5,25 +5,25 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && bash src/LiteLogo.sh")
+os.system("cd && cd alltool && bash src/LiteLogo.sh")
 
 print("")
-print("  \033[1;34m[01] \033[1;36;40mDefault  -  Standart installing and default")
-print("  \033[1;34m[02] \033[1;36;40mCoded  -  Install via encoded code NOT RECOMMENDED")
-print("  \033[1;34m[02] \033[1;36;40mQuit  -  Exit and do not download AllHackingTools")
+print("  \033[1;34m[01] \033[1;36;40mVarsayılan -  Standart kurulum ve varsayılan")
+print("  \033[1;34m[02] \033[1;36;40mKodlanmış  -  Kodlanmış kodla yükleme ÖNERİLMEZ")
+print("  \033[1;34m[02] \033[1;36;40mÇıkış -  Çık ve indirme alltool")
 
 op=int(raw_input("1nStall: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash Files/Modules.sh")
+ os.system("cd && cd alltool && bash Files/Modules.sh")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash Files/CodedModules.sh")
+ os.system("cd && cd alltool && bash Files/CodedModules.sh")
 elif(op==2):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting AllHackingTool Installer...")
+ print("\033[1;31;40malltool Kurulumcudan Çıkıyor...")
  sys.exit()
 else:
- print("\033[1;31;40mInvalid input. Quiting AllHackingTools Installer") 
+ print("\033[1;31;40mGeçersiz Giriş. alltool  Kurulumcudan çıkıyor") 
  time.sleep(0.8)
