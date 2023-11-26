e7be17f065489f2475ce0b7866dcc71afe9f169a
Meyit47 <silaatesss.31@gmail.com>
2023-11-26 23:28:03 +0300

    Update aboutMenu.py

--- src/aboutMenu.py
+++ src/aboutMenu.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools")
+os.system("cd && cd alltool")
 os.system("bash Logo.sh")
 os.system("bash src/MenuA.sh")
 
@@ -16,26 +16,26 @@ Reset="\033[0m"
 Red="\033[1;31m"
 Purple="\033[0;35m"
 
-op=int(raw_input("Options: "))
+op=int(raw_input("Seçenekler: "))
 
 if(op==1):
  os.system("cd")
- os.system("cd AllHackingTool && bash src/Inf.sh")
+ os.system("cd alltool && bash src/Inf.sh")
  os.system("python2 MainMenu.py")
 elif(op==2):
- print("\033[1;31;40mRebooting system...")
+ print("\033[1;31;40m-Sistem yeniden başlatılıyor...")
  time.sleep(0.7)
  os.system("cd")
- os.system("cd AllHackingTools")
- os.system("bash src/AllHackingTool.sh")
+ os.system("cd alltool")
+ os.system("bash src/alltool.sh")
 elif(op==3):
- print("\033[1;31;40mQuiting system...")
+ print("\033[1;31;40m-Sistemden çıkış...")
  time.sleep(1)
  sys.exit()
 else:
- print("\033[1;31;40mInvalid input. Back To MainMenu...")
+ print("\033[1;31;40mGeçersiz Giriş. Ana menüye dön...")
  time.sleep(1)
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd alltool")
  os.system("python2 MainMenu.py")
  
