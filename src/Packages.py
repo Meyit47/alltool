ac31f55012e4b810c0d583aa3685062b382763bc
Meyit47 <silaatesss.31@gmail.com>
2023-11-26 23:59:03 +0300

    Update Packages.py

--- src/Packages.py
+++ src/Packages.py
@@ -2,10 +2,10 @@ from time import sleep
 from rich.console import Console
 
 console = Console()
-tasks = [f"data {n}" for n in range(1, 3)]
+tasks = [f"veri {n}" for n in range(1, 3)]
 
-with console.status("[bold green]I perform the task ...[/bold green]") as status:
+with console.status("[koyu yeşil]Görevi ben gerçekleştiriyorum ...[/koyu yeşil]") as status:
     while tasks:
         task = tasks.pop(0)
         sleep(0.4)
-        console.log(f"{task} done!")
+        console.log(f"{task} Tamamlandı!")
