c1dce68fdd7439c72b5668d7bafba75687397a48
Meyit47 <silaatesss.31@gmail.com>
2023-11-26 23:55:21 +0300

    Update ProgressBar.py

--- src/ProgressBar.py
+++ src/ProgressBar.py
@@ -5,5 +5,5 @@ from rich.progress import track
 def do_step(step):
     sleep(random.uniform(0.001, 0.01))
 
-for step in track(range(100), description="[cyan]Processing ..."):
+for step in track(range(100), description="[cyan]İşleniyor ..."):
     do_step(step)
