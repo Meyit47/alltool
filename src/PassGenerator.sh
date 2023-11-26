9f7508ba55250680df53d0f0b4ed4eb88543851e
Meyit47 <silaatesss.31@gmail.com>
2023-11-26 23:57:48 +0300

    Update PassGenerator.sh

--- src/PassGenerator.sh
+++ src/PassGenerator.sh
@@ -4,7 +4,7 @@ b="\033[1;34m"
 w="\033[0m"
 o="\033[1;33m"
 
-echo -e $w"["$g"INFO"$w"]"$b"Easy passworlds"$w
+echo -e $w"["$g"BİLGİ"$w"]"$b"Kolay parolalar"$w
 sleep 0.1
 dd bs=12 count=1 if=/dev/urandom status=none | base32
 dd bs=12 count=1 if=/dev/urandom status=none | base32
@@ -14,8 +14,8 @@ dd bs=16 count=1 if=/dev/urandom status=none | base64
 dd bs=16 count=1 if=/dev/urandom status=none | base64
 cd
 cd
-cd AllHackingTools
+cd alltool
 python3 src/Timer1.py
 python2 src/aboutMenu.py   
 cd
-cd AllHackingTools
+cd alltool
