916f946fd7339ee9d30ed7fc222fecb4beea9042
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 01:56:48 +0300

    Update AnimationLoad2.sh

--- src/AnimationLoad2.sh
+++ src/AnimationLoad2.sh
@@ -6,113 +6,113 @@ w="\033[0m"
 o="\033[1;33m"
 printf "\e[1;92m"
 
-printf "\n#aaaaaaaaaaaaaaaaaaaaa Loading .\n"
+printf "\n#aaaaaaaaaaaaaaaaaaaaa Yükleniyor .\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
+sleep 0.01
+clear
+printf "\n#aaaaaaaaaaaaaaaaaaaaa Yükleniyor ..\n"
 printf "\nPlease wait a moment ..!\n"
 sleep 0.01
 clear
-printf "\n#aaaaaaaaaaaaaaaaaaaaa Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n##aaaaaaaaaaaaaaaaaaaa Yükleniyor ...\n"
 sleep 0.01
 clear
-printf "\n##aaaaaaaaaaaaaaaaaaaa Loading ...\n"
-sleep 0.01
-clear
-printf "\n##aaaaaaaaaaaaaaaaaaaa Loading .\n"
+printf "\n##aaaaaaaaaaaaaaaaaaaa Yükleniyor .\n"
 sleep 0.05
 clear
-printf "\n###aaaaaaaaaaaaaaaaaaa Loading ..\n"
+printf "\n###aaaaaaaaaaaaaaaaaaa Yükleniyor ..\n"
 clear
-printf "\n###aaaaaaaaaaaaaaaaaaa Loading ...\n"
+printf "\n###aaaaaaaaaaaaaaaaaaa Yükleniyor ...\n"
 clear
-printf "\n####aaaaaaaaaaaaaaaaaa Loading .\n"
+printf "\n####aaaaaaaaaaaaaaaaaa Yükleniyor .\n"
 sleep 0.01
 clear
-printf "\n####aaaaaaaaaaaaaaaaaa Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n####aaaaaaaaaaaaaaaaaa Yükleniyor ..\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n#####aaaaaaaaaaaaaaaaa Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n#####aaaaaaaaaaaaaaaaa Yükleniyor ...\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n#####aaaaaaaaaaaaaaaaa Loading .\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n#####aaaaaaaaaaaaaaaaa Yükleniyor .\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.02
 clear
-printf "\n######aaaaaaaaaaaaaaaa Loading ..\n"
+printf "\n######aaaaaaaaaaaaaaaa Yükleniyor ..\n"
 sleep 0.02
 clear
-printf "\n#######aaaaaaaaaaaaaaa Loading ...\n"
+printf "\n#######aaaaaaaaaaaaaaa Yükleniyor ...\n"
 sleep 0.01
 clear
-printf "\n#######aaaaaaaaaaaaaaa Loading .\n"
+printf "\n#######aaaaaaaaaaaaaaa Yükleniyor .\n"
 sleep 0.01
 clear
-printf "\n#########aaaaaaaaaaaaa Loading ..\n"
+printf "\n#########aaaaaaaaaaaaa Yükleniyor ..\n"
 clear
-printf "\n##########aaaaaaaaaaaa Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n##########aaaaaaaaaaaa Yükleniyor ...\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n###########aaaaaaaaaaa Loading .\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n###########aaaaaaaaaaa Yükleniyor.\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n###########aaaaaaaaaaa Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n###########aaaaaaaaaaa Yükleniyor..\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n############aaaaaaaaaa Loading ...\n"
+printf "\n############aaaaaaaaaa Yükleniyor...\n"
 sleep 0.03
 clear
-printf "\n#############aaaaaaaaa Loading .\n"
+printf "\n#############aaaaaaaaa Yükleniyor .\n"
 sleep 0.01
 clear
-printf "\n##############aaaaaaaa Loading ..\n"
+printf "\n##############aaaaaaaa Yükleniyor ..\n"
 sleep 0.05
 clear
-printf "\n###############aaaaaaa Loading ...\n"
+printf "\n###############aaaaaaa Yükleniyor ...\n"
 clear
-printf "\n################aaaaaa Loading .\n"
+printf "\n################aaaaaa Yükleniyor .\n"
 sleep 0.005
 clear
-printf "\n################aaaaaa Loading ..\n"
+printf "\n################aaaaaa Yükleniyor ..\n"
 sleep 0.01
 clear
-printf "\n#################aaaaa Loading ...\n"
+printf "\n#################aaaaa Yükleniyor ...\n"
 sleep 0.01
 clear
-printf "\n#################aaaaa Loading .\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n#################aaaaa Yükleniyor .\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n##################aaaa Loading ..\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n##################aaaa Yükleniyor ..\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
 help
-printf "\n###################aaa Loading ...\n"
-printf "\nPlease wait a moment ..!\n"
+printf "\n###################aaa Yükleniyor ...\n"
+printf "\nBir süre bekleyin lütfen ..!\n"
 sleep 0.01
 clear
-printf "\n###################aaa Loading .\n"
+printf "\n###################aaa Yükleniyor .\n"
 sleep 0.006
 clear
-printf "\n###################aaa Loading ..\n"
+printf "\n###################aaa Yükleniyor ..\n"
 sleep 0.006
 clear
-printf "\n####################aa Loading ...\n"
+printf "\n####################aa Yükleniyor ...\n"
 leep 0.1
 clear
-printf "\n###################### DONE!\n"
+printf "\n###################### TAMAMLANDI!\n"
 cd
 cd 
-cd AllHackingTools 
+cd alltool 
 bash src/CheckWifi.sh
 sleep 2
 clear
 sleep 0.1
 cd
 cd
-cd AllHackingTools
+cd alltool
 python3 .check/Configuration.py
