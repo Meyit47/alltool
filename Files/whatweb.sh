28a3b087693c63ebd4d32e4af60b475f43819ec1
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 03:10:35 +0300

    Update whatweb.sh

--- Files/whatweb.sh
+++ Files/whatweb.sh
@@ -5,15 +5,15 @@ w="\033[0m"
 
 clear
 cd
-cd AllHackingTools
+cd alltool
 cd WhatWeb
 clear
 ./whatweb
-echo "Please enter all your required settings to run the program, for example: reddit.com"
-echo -e $b">>>"$w" Consoler "$g"WhatWeb"$w
+echo "Programı çalıştırmak için lütfen gerekli ayarlarınızı girin, örneğin: reddit.com"
+echo -e $b">>>"$w" consoller "$g"WhatWeb"$w
 read opt
 sleep 0.1
 ./whatweb $opt
 cd
 cd
-cd AllHackingTools
+cd alltool
