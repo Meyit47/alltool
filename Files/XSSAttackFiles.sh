b9ea73efffa85bcdf0a7faf9868fa296030c42bf
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 03:34:51 +0300

    Update XSSAttackFiles.sh

--- Files/XSSAttackFiles.sh
+++ Files/XSSAttackFiles.sh
@@ -1,18 +1,18 @@
 cd
 cd
-cd AllHackingTools
+cd alltool
 pip install bs4
 git clone https://github.com/menkrep1337/XSSCon
 chmod 755 -R XSSCon
 cd XSSCon
 cd
 cd
-cd AllHackingTools
+cd alltool
 git clone https://github.com/Ekultek/XanXSS
 cd
 cd
-cd AllHackingTools
+cd alltool
 git clone https://github.com/s0md3v/XSStrike
 cd
 cd 
-cd AllHackingTools
+cd alltool
