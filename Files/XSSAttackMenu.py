54049edea4b18c0a79dbea20a915ad91793d7a52
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 03:33:21 +0300

    Update XSSAttackMenu.py

--- Files/XSSAttackMenu.py
+++ Files/XSSAttackMenu.py
@@ -5,36 +5,36 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd alltool && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mXSSCon - Simple XSS Scanner tool")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mXanXSS - A simple XSS finding tool")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mXSStrike - Most advanced XSS scanner")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System - log out AllHackingTools")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mXSSCon - Basit XSS Tarayıcı aracı")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mXanXSS - Basit bir XSS bulma aracı")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mXSStrike - En gelişmiş XSS tarayıcı")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mSistemden çıkış - alltool dan çıkış yap)
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mAna menüye dön")
 
 op=int(raw_input("XSSattACk: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Files && bash XSScon.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Files && bash XSScon.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Files && bash XanXSS.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Files && bash XanXSS.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Files && bash XSStrike.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Files && bash XSStrike.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==4):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting System...")
+ print("\033[1;31;40mSistemden Çıkış...")
  sys.exit()
 elif(op==5):
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd alltool")
  os.system("python2 MainMenu.py")
 else:
- print("\033[1;31;40mInvalid input. Reloading Tools") 
+ print("\033[1;31;40mGeçersiz Giriş.Araçlar Yeniden Yüklenmeli.") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 Files/AnalistickMenu.py")
