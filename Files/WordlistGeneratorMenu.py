22d672aec22fb075b3681ccd44361b6ea6140399
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 03:40:13 +0300

    Update WordlistGeneratorMenu.py

--- Files/WordlistGeneratorMenu.py
+++ Files/WordlistGeneratorMenu.py
@@ -5,36 +5,36 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd alltool && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mWlcreator - Wordlist creator written in C")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mGoblinWordGenerator - Useful wordlist generator in python")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mSMWYG - tool allows you to perform OSINT and reconnaissance")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System - log out AllHackingTools")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mWlcreator - Kelime listesi yaratıcısı yazılı C")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mGoblinWordGenerator - Python'da kullanışlı kelime listesi oluşturur")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mSMWYG - araç OSINT ve keşif yapmanızı sağlar")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mSistemden Çıkış - alltool dan çıkış")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mAna menüye dön")
 
 op=int(raw_input("Ma1lHacK: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd wlcreator && ./wlcreator 5 && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd wlcreator && ./wlcreator 5 && cd && cd alltool && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd GoblinWordGenerator && python3 goblin.py && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd GoblinWordGenerator && python3 goblin.py && cd && cd alltool && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd SMWYG-Show-Me-What-You-Got && python3 SMWYG.py && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd SMWYG-Show-Me-What-You-Got && python3 SMWYG.py && cd && cd alltool && python2 MainMenu.py")
 elif(op==4):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting utility...")
+ print("\033[1;31;40mSistemdem çıkış...")
  sys.exit()
 elif(op==5):
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 MainMenu.py")
 else:
- print("\033[1;31;40mInvalid input. Reloading Tools") 
+ print("\033[1;31;40mGeçersiz Giriş.Araçlar Yeniden Yüklenmeli.") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 Files/AndroidMenu.py")
