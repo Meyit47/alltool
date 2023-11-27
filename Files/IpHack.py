b245796152ffb1b8669bf899ce8922b67f79916b
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 06:06:25 +0300

    Update IpHack.py

--- Files/IpHack.py
+++ Files/IpHack.py
@@ -1,17 +1,17 @@
 from iphack import *
 
-print("01: Ip Address")
-print("02: Proxy Checker")
-print("03: Proxy Generator")
+print("01: IP adresi")
+print("02: Proxy Denetleyicisi")
+print("03: Proxy Oluşturucu")
 
-op = input("Select Option: ")
+op = input("Seçeneği Seçin: ")
 
 if op == "1" or op == "01":
-    ip_adr = input("While IP: ")
+    ip_adr = input("IP iken: ")
     ip.address(ip_adr)
 if op == "2" or op == "02":
-    ip_p = input("While proxy ip: ")
-    port_p = input("While proxy port: ")
+    ip_p = input("Proxy ip iken: ")
+    port_p = input("Proxy bağlantı noktası iken: ")
     check.proxy(ip_p, port_p)
 if op == "3" or op == "03":
     ip.proxy(False)
