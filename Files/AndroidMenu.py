8a9b456347ff88aee1b261507b30ace2687a2199
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 06:25:52 +0300

    Update AndroidMenu.py

--- Files/AndroidMenu.py
+++ Files/AndroidMenu.py
@@ -5,40 +5,40 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd alltool && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mPyshell - Remote Access Trojan - RAT")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mWishfish - Powerful Tool For Grab Front Camera Snap Using A Link")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mLockPhish - Tool for phishing attacks on the lock screen")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mLockPhish - Tool for phishing attacks on the lock screen")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System - Log out AllHackingTools")
-print("  \033[1;34m[ 06 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mPyshell - Uzaktan Erişim Truva Atı - RAT")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mWishfish - Bir Bağlantı Kullanarak Ön Kamera Fotoğrafını Yakalamak İçin Güçlü Araç")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mLockPhish - Kilit ekranında kimlik avı saldırılarına yönelik araç")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mLockPhish - Kilit ekranında kimlik avı saldırılarına yönelik araç")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mExit System - alltool dan çıkış")
+print("  \033[1;34m[ 06 ] >> \033[1;36;40mAna menüye dön")
 
 op=int(raw_input("R6moteTRo1anR8t: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd pyshell && ./Pyshell && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd pyshell && ./Pyshell && cd && cd alltool && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd wishfish && ./wishfish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd wishfish && ./wishfish.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd lockphish && bash lockphish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd lockphish && bash lockphish.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Files && bash HatCloud.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Files && bash HatCloud.sh && cd && cd alltool && python2 MainMenu.py")
 elif(op==5):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting System...")
+ print("\033[1;31;40mSistemden çıkış...")
  sys.exit()
 elif(op==6):
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 MainMenu.py")
 else:
- print("\033[1;31;40mInvalid input. Reloading Tools") 
+ print("\033[1;31;40mGeçersiz Giriş.Araçlar Yeniden Yüklenmeli.") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 Files/AndroidMenu.py")
