b7cdbdc439503703d708e62e3f61b006a6df4b28
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 05:06:03 +0300

    Update SQLinjectionMenu.py

--- Files/SQLinjectionMenu.py
+++ Files/SQLinjectionMenu.py
@@ -5,41 +5,41 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd alltool && clear && bash Logo.sh")
 
-print("  \033[1;34m[ 01 ] >> \033[1;36;40mVirusInjection - create a virus in /sdcard/")
-print("  \033[1;34m[ 02 ] >> \033[1;36;40mDebinject - Inject malicious code into *.debs")
-print("  \033[1;34m[ 03 ] >> \033[1;36;40mInfect - Create virus for windows, android")
-print("  \033[1;34m[ 04 ] >> \033[1;36;40mExit System")
-print("  \033[1;34m[ 05 ] >> \033[1;36;40mBack To MainMenu")
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mVirusInjection - /sdcard/ a virüs oluştur.")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mDebinject - Kötü amaçlı kodu enjekte edin *.debs")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mInfect - Windows, android için virüs oluşturma")
+print("  \033[1;34m[ 04 ] >> \033[1;36;40mSistemden çıkış")
+print("  \033[1;34m[ 05 ] >> \033[1;36;40mAna menüye dön")
 
 op=int(raw_input("Iject1onTo0l: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Vitus2.0 && cp VirtualXposed.apk /sdcard/")
- print("Your virus has been created!")
+ os.system("cd && cd alltool && cd Vitus2.0 && cp VirtualXposed.apk /sdcard/")
+ print("Virüsünüz oluşturuldu!")
  time.sleep(1)
- print("Find Your virus in /sdcard/")
+ print("Virüsünüzü /sdcard/'da bulun")
  time.sleep(2)
- os.system("cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Debinject && python2 debinject.py && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Debinject && python2 debinject.py && cd && cd alltool && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Infect && Infect && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd alltool && cd Infect && Infect && cd && cd alltool && python2 MainMenu.py")
 elif(op==4):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting utility...")
+ print("\033[1;31;40mSistemden çıkış...")
  sys.exit()
 elif(op==5):
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 MainMenu.py")
 else:
- print("\033[1;31;40mInvalid input. Reloading Tools") 
+ print("\033[1;31;40mGeçersiz Giriş.Araçlar Yeniden Yüklenmeli.") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd alltool")
  os.system("python2 Files/SQLinjectionMenu.py")
