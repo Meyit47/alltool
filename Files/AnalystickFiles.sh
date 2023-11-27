5a7d783f1972b6032a1f6f666ba042d17f0b0be8
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 06:27:06 +0300

    Update AnalystickFiles.sh

--- Files/AnalystickFiles.sh
+++ Files/AnalystickFiles.sh
@@ -1,17 +1,17 @@
 cd
 cd 
-cd AllHackingTools
+cd alltool
 pip2 install termcolor
 git clone https://github.com/floriankunushevci/rang3r
 cd
-cd AllHackingTools
+cd alltool
 git clone https://github.com/TechnicalMujeeb/TM-scanner.git 
 cd TM-scanner 
 chmod +x * sh install.sh
 cd
 cd
-cd AllHackingTools
+cd alltool
 git clone https://github.com/Gameye98/AstraNmap
 cd
 cd
-cd AllHackingTools
+cd alltool
