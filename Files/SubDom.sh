903655eb242e5b3846ed1b8c02d57c6a6481973f
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 04:36:47 +0300

    Update SubDom.sh

--- Files/SubDom.sh
+++ Files/SubDom.sh
@@ -5,15 +5,15 @@ w="\033[0m"
 
 clear
 cd
-cd AllHackingTools
+cd alltool
 bash src/RunLogo.sh
 cd takeover
 echo ""
-echo -e $b">>>"$w" Write the site address: "$g"TakeOver"$w
+echo -e $b">>>"$w" Site adresini yazın: "$g"Devralmak"$w
 read siteURL
 sleep 0.1
-echo -e $b">>>"$w" Please wait a moment: "$g"TakeOver"$w
+echo -e $b">>>"$w" Bir süre bekleyin lütfen: "$g"Devralmak"$w
 sleep 0.6
 python3 takeover.py -d $siteURL -v
 cd
-cd AllHackingTools
+cd alltool
