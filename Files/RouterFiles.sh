03d3f4f1c1644715e0e5836f64fd9f5721574d95
Meyit47 <silaatesss.31@gmail.com>
2023-11-27 05:12:05 +0300

    Update RouterFiles.sh

--- Files/RouterFiles.sh
+++ Files/RouterFiles.sh
@@ -5,35 +5,35 @@ apt-get install paramiko
 apt-get install pysnmp
 apt-get install pycrypto
 cd
-cd AllHackingTools
+cd alltool
 apt-get install python3-pip
 git clone https://www.github.com/threat9/routersploit
 cd routersploit
 python3 -m pip install -r requirements.txt
 cd
 cd
-cd AllHackingTools
+cd alltool
 git clone https://github.com/commixproject/commix.git commix     
 cd commix
 sudo python setup.py install
 cd 
-cd AllHackingTools 
+cd alltool 
 git clone https://github.com/kuburan/txtool.git  
 cd txtool   
 ./install.py 
 cd 
 cd 
-cd AllHackingTools
+cd alltool
 git clone https://github.com/Moham3dRiahi/XAttacker.git
 cd XAttacker
 chmod +x termux-install.sh
 bash termux-install.sh
 cd
 cd 
-cd AllHackingTools
+cd alltool
 git clone https://github.com/karjok/fim
 cd fim 
 pip install -r requirements.txt
 cd
 cd
-cd AllHackingTools
+cd alltool
